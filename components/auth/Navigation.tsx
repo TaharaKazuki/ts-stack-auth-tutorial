b8a3766f8fbd398f5b348d4ffc148524f42d3ffc
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-13 22:00:00 +0900

    update

--- components/auth/Navigation.tsx
+++ components/auth/Navigation.tsx
@@ -6,3 +6,13 @@ import Link from 'next/link';
 interface NavigationProps {
   user: any;
 }
+
+const Navigation = () => {
+  return (
+    <header className="shadow-lg shadow-gray-100 mb-10">
+      <div className="container mx-auto flex max-w-screen-md items-center justify-between"></div>
+    </header>
+  );
+};
+
+export default Navigation;
