1c8cafbf5c229b9b22bc0758cd281fe957d71309
Tahara Kazuki <ssp0727.lnc@gmail.com>
2024-05-14 21:54:29 +0900

    update

--- components/auth/Navigation.tsx
+++ components/auth/Navigation.tsx
@@ -10,7 +10,19 @@ interface NavigationProps {
 const Navigation = () => {
   return (
     <header className="shadow-lg shadow-gray-100 mb-10">
-      <div className="container mx-auto flex max-w-screen-md items-center justify-between"></div>
+      <div className="container mx-auto flex max-w-screen-md items-center justify-between px-1 py-3">
+        <Link href="/" className="cursor-pointer text-xl font-bold">
+          T3 Stack 入門
+        </Link>
+      </div>
+      <div className="flex items-center space-x-1">
+        <Button asChild variant="ghost" className="font-bold">
+          <Link href="/login">ログイン</Link>
+        </Button>
+        <Button asChild variant="default" className="font-bold">
+          <Link href="signup">新規登録</Link>
+        </Button>
+      </div>
     </header>
   );
 };
